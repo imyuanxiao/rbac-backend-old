21fac953b7f18be99def5fbdbbc8d46921bd787b
imyuanxiao <imyuanxiao@126.com>
2023-05-03 00:51:56 +0100

    在application.yaml里配置数据库信息

--- src/main/java/com/imyuanxiao/rbac/RbacApplication.java
+++ src/main/java/com/imyuanxiao/rbac/RbacApplication.java
@@ -1,9 +1,11 @@
 package com.imyuanxiao.rbac;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.imyuanxiao.rbac.mapper")
 public class RbacApplication {
 
     public static void main(String[] args) {
