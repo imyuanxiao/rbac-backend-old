7a33883b64cc852a67b01c7c2ad84a59684c9967
imyuanxiao <imyuanxiao@126.com>
2023-05-03 15:32:15 +0100

    配置mybatisPlus分页插件

---
+++ src/main/java/com/imyuanxiao/rbac/config/MybatisPlusConfig.java
@@ -0,0 +1,29 @@
+package com.imyuanxiao.rbac.config;
+
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: imyuanxiao
+ * @Date: 2023/5/3 15:17
+ */
+@Configuration
+@MapperScan("com.imyuanxiao.rbac.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件,一缓和二缓遵循mybatis的规则
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+
+}
