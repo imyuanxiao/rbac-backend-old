34dbeb7a82554907e2618ad085de1a8fd8b86bc3
imyuanxiao <imyuanxiao@126.com>
2023-05-07 10:53:49 +0100

    调整参数验证依赖包

--- src/main/java/com/imyuanxiao/rbac/model/param/LoginParam.java
+++ src/main/java/com/imyuanxiao/rbac/model/param/LoginParam.java
@@ -3,6 +3,8 @@ package com.imyuanxiao.rbac.model.param;
 import com.imyuanxiao.rbac.annotation.ExceptionCode;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
 
@@ -18,15 +20,11 @@ public class LoginParam {
     @ExceptionCode(value = 100001, message = "账号验证错误")
     private String username;
 
-    @Length(min = 8, max = 20, message = "手机号长度为8-20位")
-    @ExceptionCode(value = 100002, message = "手机号验证错误")
     private String phone;
 
-    @NotEmpty(message = "密码不能为空")
+    @NotBlank(message = "密码不能为空")
     @Length(min = 4, max = 12, message = "密码长度为4-12位")
     @ExceptionCode(value = 100003, message = "密码验证错误")
     private String password;
-
-
 
 }
